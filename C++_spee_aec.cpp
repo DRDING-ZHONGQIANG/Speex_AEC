f7e932d9886bcdfe15533a5b1f0bb96e46dbdc11
Dr_DING <57161979+DRDING-ZHONGQIANG@users.noreply.github.com>
2022-08-24 09:38:01 +0800

    Add files via upload

---
+++ C++_spee_aec.cpp
@@ -0,0 +1,100 @@
+#ifdef HAVE_CONFIG_H
+#include "config.h"
+#endif
+
+#include "speex/speex_echo.h"
+#include "speex/speex_preprocess.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <cstdint>
+
+using std::cin;
+using std::cout;
+using std::endl;
+using std::fstream;
+using std::string;
+
+typedef struct  WAV_HEADER
+{
+    /* RIFF Chunk Descriptor */
+    uint8_t         RIFF[4];        // RIFF Header Magic header
+    uint32_t        ChunkSize;      // RIFF Chunk Size
+    uint8_t         WAVE[4];        // WAVE Header
+    /* "fmt" sub-chunk */
+    uint8_t         fmt[4];         // FMT header
+    uint32_t        Subchunk1Size;  // Size of the fmt chunk
+    uint16_t        AudioFormat;    // Audio format 1=PCM,6=mulaw,7=alaw,     257=IBM Mu-Law, 258=IBM A-Law, 259=ADPCM
+    uint16_t        NumOfChan;      // Number of channels 1=Mono 2=Sterio
+    uint32_t        SamplesPerSec;  // Sampling Frequency in Hz
+    uint32_t        bytesPerSec;    // bytes per second
+    uint16_t        blockAlign;     // 2=16-bit mono, 4=16-bit stereo
+    uint16_t        bitsPerSample;  // Number of bits per sample
+    /* "data" sub-chunk */
+    uint8_t         Subchunk2ID[4]; // "data"  string
+    uint32_t        Subchunk2Size;  // Sampled data length
+} wav_hdr;
+
+#define NN   128
+#define TAIL 9068
+
+wav_hdr  wavHeader;
+int headerSize = sizeof(wav_hdr), filelength = 0;
+size_t bytesRead;
+
+int main(int argc, char **argv)
+{
+   FILE *echo_fd, *ref_fd, *e_fd;
+   short echo_buf[NN], ref_buf[NN], e_buf[NN];
+   SpeexEchoState *st;
+   SpeexPreprocessState *den;
+   int sampleRate = 16000;
+
+   fprintf(stderr, "test\n");
+
+   /*
+   if (argc != 4)
+   {
+      fprintf(stderr, "testecho mic_signal.sw speaker_signal.sw output.sw\n");
+      exit(1);
+   }
+   */
+
+   //char echo_path[200] = "/home/test/PycharmProjects/speexdsp/libspeexdsp/HS_Speech_1_mic_edit_16k.wav";
+   //ref_path  = "/home/test/PycharmProjects/speexdsp/libspeexdsp/Speech_1_lpb_edit_16k.wav";
+   //e_path    = "/home/test/PycharmProjects/speexdsp/libspeexdsp/Speech_1_enh__16k.wav";
+
+   echo_fd   = fopen("/home/test/PycharmProjects/speexdsp/libspeexdsp/HS_Speech_1_mic_edit_16k.wav", "r");
+   bytesRead = fread(&wavHeader, 1, headerSize, echo_fd);
+   ref_fd    = fopen("/home/test/PycharmProjects/speexdsp/libspeexdsp/Speech_1_lpb_edit_16k.wav", "r");
+   bytesRead = fread(&wavHeader, 1, headerSize, ref_fd);
+   e_fd      = fopen("/home/test/PycharmProjects/speexdsp/libspeexdsp/Speech_1_enh__16k.wav", "w");
+   fwrite(&wavHeader, 1, headerSize, e_fd);
+
+
+   st        = speex_echo_state_init(NN, TAIL);
+   den       = speex_preprocess_state_init(NN, sampleRate);
+   speex_echo_ctl(st, SPEEX_ECHO_SET_SAMPLING_RATE, &sampleRate);
+   speex_preprocess_ctl(den, SPEEX_PREPROCESS_SET_ECHO_STATE, st);
+
+   //while (!feof(ref_fd) && !feof(echo_fd))
+   while (!feof(ref_fd))
+   {
+      fread(ref_buf, sizeof(short),  NN, ref_fd);
+      fread(echo_buf, sizeof(short), NN, echo_fd);
+      speex_echo_cancellation(st, echo_buf, ref_buf, e_buf);
+      speex_preprocess_run(den, e_buf);
+      fwrite(e_buf, sizeof(short), NN, e_fd);
+   }
+   speex_echo_state_destroy(st);
+   speex_preprocess_state_destroy(den);
+   fclose(e_fd);
+   fclose(echo_fd);
+   fclose(ref_fd);
+   return 0;
+}
